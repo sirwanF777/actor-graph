a05e9f53d321ffc80b99dca0a16c9a1eb3ea0df1
sirwanF777 <sirwan.farajpanah79@gmail.com>
2024-01-27 22:57:46 +0330

    1: Creating the actor_actor function 2: Adding a database that links actors who have played with each other.

--- data_scrubbing.py
+++ data_scrubbing.py
@@ -1,22 +1,50 @@
 import pymongo
 from config import HOST_PORT, DATABASE_NAME, COLLECTION_NAME
-
+# example: HOST_PORT = "localhost:27017"; DATABASE_NAME = "....."; COLLECTION_NAME = "...."
 
 FILM_STARS = {}
 FILM_IMDB = {}
 ACTORS_FILM = {}
 ACTORS_DIRECTOR = {}
+ACTOR_ACTOR = {}
 ACTORS = []
 FILMS = []
 DIRECTORS = []
 
 
 def _get_datas():
+    """
+    In this function, we load the movie information using the pymongo library. Then we
+    extract the following information from these loaded data:
+
+    1: ACTORS_FILM: Here we have shown which movies each actor has acted in. Its format is:
+    {
+    "actor1": ["film1", "film2", ...],
+    "actor2": ["film1", "film3", ...],
+    ...
+    }
+
+    2: ACTORS_DIRECTOR: Here we show which directors each actor has worked with. Its format is:
+    {
+    "actor1": ["director1", "directo2", ...],
+    "actor2": ["director1", "director3", ...],
+    ...
+    }
+
+    3: ACTOR_ACTOR: Here we show which actors each actor has co-starred with. Its format is:
+    {
+    "actor1": ["actor2", "actor3", ...],
+    "actor2": ["actor1", ...],
+    "actor3": ["actor1", ...],
+    ...
+    }
+    """
+
     client = pymongo.MongoClient(HOST_PORT)
     db = client[DATABASE_NAME]
     collection = db[COLLECTION_NAME]
 
-    global ACTORS, DIRECTORS, FILMS, ACTORS_DIRECTOR, ACTORS_FILM, FILM_IMDB, FILM_STARS
+    global ACTORS, DIRECTORS, FILMS, ACTORS_DIRECTOR, ACTORS_FILM, FILM_IMDB, FILM_STARS, ACTOR_ACTOR
 
     result = collection.find()
 
@@ -39,11 +67,23 @@ def _get_datas():
             else:
                 ACTORS_FILM[actor].append(data["name"])
 
+            if actor not in ACTOR_ACTOR.keys():
+                t = actors.copy()
+                t.remove(actor)
+                ACTOR_ACTOR[actor] = t
+            else:
+                t = actors.copy()
+                t.remove(actor)
+                ACTOR_ACTOR[actor].extend(t)
+
             if data['director'] != None:
                 if actor not in ACTORS_DIRECTOR.keys():
                     ACTORS_DIRECTOR[actor] = [data["director"]]
                 elif data["director"] not in ACTORS_DIRECTOR[actor]:
                     ACTORS_DIRECTOR[actor].append(data["director"])
+
+    for actor in ACTOR_ACTOR.keys():
+        ACTOR_ACTOR[actor] = set(ACTOR_ACTOR[actor])
 
     ACTORS = set(n_actors)
     DIRECTORS = set(n_director)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import json
 
-from data_scrubbing import ACTORS_FILM, ACTORS_DIRECTOR, FILMS, DIRECTORS
+from data_scrubbing import ACTORS_FILM, ACTORS_DIRECTOR, ACTOR_ACTOR
 import pandas as pd
 
 
 def actor_actor_film():
+    """
+    In this function, using the ACTORS_FILM data, we can find the relationship
+    between the actors using the films they participated in.
+    And we save the extracted data in csv and json formats in the archive/actor_actor_film path.
+    """
     communication_actors = {}
 
     for actor1, films1 in ACTORS_FILM.items():
@@ -31,10 +36,15 @@ def actor_actor_film():
     json_file_path = "archive/actor_actor_film/actors_relations.json"
     with open(json_file_path, mode='w', encoding='utf-8') as file:
         json.dump(communication_actors, file, ensure_ascii=False, indent=2)
-    print("Data saved in json format successfully.")
+    print(f"Data saved in json format successfully.\n{"__"*25}")
 
 
 def actor_actor_director():
+    """
+    In this function, using ACTORS_DIRECTOR data, we can find the relationship
+    between actors using the director they worked with.
+    And we save the extracted data in csv and json formats in the archive/actor_actor_director path.
+    """
     communication_actors = {}
 
     for actor1, director1 in ACTORS_DIRECTOR.items():
@@ -62,8 +72,46 @@ def actor_actor_director():
     json_file_path = "archive/actor_actor_director/actors_relations.json"
     with open(json_file_path, mode='w', encoding='utf-8') as file:
         json.dump(communication_actors, file, ensure_ascii=False, indent=2)
-    print("Data saved in json format successfully.")
+    print(f"Data saved in json format successfully.\n{"__"*25}")
+
+
+def actor_actor():
+    """
+    In this function, by using the ACTOR_ACTOR data, we can use the data to show the
+    relationship of the actors through which actors have collaborated together.
+    And we save the extracted data in csv and json formats in the archive/actor_actor path.
+    """
+    communication_actors = {}
+
+    for actor1, actor1_re in ACTOR_ACTOR.items():
+        for actor2, actor2_re in ACTOR_ACTOR.items():
+            common_actors = set(actor1_re).intersection(actor2_re)
+            if common_actors and actor1 != actor2:
+                if actor1 in communication_actors.keys():
+                    communication_actors[actor1].append(actor2)
+                else:
+                    communication_actors[actor1] = []
+
+    node_size = len(communication_actors.keys())
+    edge_size = sum([len(i) for i in communication_actors.values()])
+    print(f"node size: {node_size}\nedge size: {edge_size}\n")
+
+    # save file "csv"
+    df = pd.DataFrame(
+        [(actor, relation) for actor, relations in communication_actors.items() for relation in relations],
+        columns=["actor", "Relationship"])
+    csv_file_path = "archive/actor_actor/actors_relations.csv"
+    df.to_csv(csv_file_path, index=False, encoding='utf-8')
+    print("Data saved in csv format successfully.")
+
+    # save file json
+    json_file_path = "archive/actor_actor/actors_relations.json"
+    with open(json_file_path, mode='w', encoding='utf-8') as file:
+        json.dump(communication_actors, file, ensure_ascii=False, indent=2)
+    print(f"Data saved in json format successfully.\n{"__"*25}")
 
 
 if __name__ == "__main__":
+    actor_actor_film()
     actor_actor_director()
+    actor_actor()
